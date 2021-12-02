a1ccc853f087ee66841b9765260e761a0d487903
Ryan Majeski <ryanmajeski@gmail.com>
2021-12-02 15:41:20 -0500

    bevy website tutorial overview

---
+++ bevy_tutorial/src/main.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}
+
+struct Person;
+
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Ryan".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Emily".to_string()));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
